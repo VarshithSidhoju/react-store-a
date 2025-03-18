7968faee3552f9fda495d4dc06fb6453d01d36ff
VarshithSidhoju <varshithsidhoju@gmail.com>
2025-03-18 10:10:23 +0530

    initial

---
+++ src/components/Products.jsx
@@ -0,0 +1,24 @@
+import "./Products.css"
+export default function Products() {
+  const products = [
+    { id: 1, name: "Product 1", price: 30 },
+    { id: 2, name: "Product 2", price: 40 },
+    { id: 3, name: "Product 3", price: 45 },
+    { id: 1, name: "Product 1", price: 30 },
+    { id: 2, name: "Product 2", price: 40 },
+    { id: 3, name: "Product 3", price: 45 },
+  ];
+  return (
+    <div>
+      <div className="App-Products-Row">
+        {products.map((value, index) => (
+          <div className="App-Products-Box" key={index}>
+            <h3>{value.name}</h3>
+            <h4>{value.price}</h4>
+            <button>Add to cart</button>
+            </div>
+        ))}
+      </div>
+    </div>
+  );
+}
